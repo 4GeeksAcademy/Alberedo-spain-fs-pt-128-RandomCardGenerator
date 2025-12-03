5e7c5c6e847e21e7a7c2e0d9b140d19028676aba
Alberto Madrid <albertomadrid93@gmail.com>
2025-12-03 20:47:29 +0000

    Proyecto finalizado con extras

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const palos = ["♦", "♥", "♠", "♣"]
 const valores = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 const rojas = ["♦", "♥"]
+let barajadoAutomatico = false
 
 function cartaAleatoria() {
   let paloAleatorio = Math.floor(Math.random() * palos.length)
@@ -14,7 +15,7 @@ function cartaAleatoria() {
     elem.textContent = palos[paloAleatorio]
     if (rojas.includes(palos[paloAleatorio])) {
       elem.classList.add("text-danger")
-    }else{
+    } else {
       elem.classList.remove("text-danger")
     }
   }
@@ -27,6 +28,31 @@ function barajaCarta() {
     clearInterval(barajando);
   }, 2000);
 }
+
+const carta = document.querySelector(".card")
+carta.addEventListener("click", function () {
+  barajaCarta()
+})
+
+const selectorAltura = document.querySelector("#alturaCard")
+selectorAltura.addEventListener("input", function () {
+  carta.style.height = `${selectorAltura.value}px`
+})
+
+const selectorAnchura = document.querySelector("#anchoCard")
+selectorAnchura.addEventListener("input", function () {
+  carta.style.width = `${selectorAnchura.value}px`
+})
+
+const switchBarajadoAuto = document.querySelector("#barajadoAuto")
+switchBarajadoAuto.addEventListener("change", function(){
+if(switchBarajadoAuto.checked ==true){
+  const autoBarajador = setInterval(barajaCarta, 10000);
+}else{
+  clearInterval(autoBarajador)
+}
+  
+})
 
 window.onload = function () {
 
