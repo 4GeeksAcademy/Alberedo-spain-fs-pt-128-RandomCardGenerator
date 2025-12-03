44a8b252ad2f5d335be886f63b978284c1e33879
Alberto Madrid <albertomadrid93@gmail.com>
2025-12-03 19:31:00 +0000

    Parte basica terminada con algun extra

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-import "bootstrap";
-import "./style.css";
+const palos = ["♦", "♥", "♠", "♣"]
+const valores = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+const rojas = ["♦", "♥"]
+
+function cartaAleatoria() {
+  let paloAleatorio = Math.floor(Math.random() * palos.length)
+  let numeroAleatorio = Math.floor(Math.random() * valores.length)
+
+  let numero = document.querySelector(".numero")
+  numero.textContent = valores[numeroAleatorio]
+
+  let palo = document.querySelectorAll(".palos")
+  for (let elem of palo) {
+    elem.textContent = palos[paloAleatorio]
+    if (rojas.includes(palos[paloAleatorio])) {
+      elem.classList.add("text-danger")
+    }else{
+      elem.classList.remove("text-danger")
+    }
+  }
+}
+
+function barajaCarta() {
+  const barajando = setInterval(cartaAleatoria, 50);
+
+  setTimeout(() => {
+    clearInterval(barajando);
+  }, 2000);
+}
+
+window.onload = function () {
+
+  barajaCarta()
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+
 };
